9989679397188d53d895505f16f95ef1f028977d
Andrey Nagatani <Andrey Nagatani>
2016-11-29 00:26:08 -0200

    corrigido venda, estoque.quantidade sofre baixa após a venda agora

--- src/bra/ifsp/farmasisava/bd/JDBCClienteDao.java
+++ src/bra/ifsp/farmasisava/bd/JDBCClienteDao.java
@@ -8,6 +8,7 @@ import java.util.Date;
 
 
 public class JDBCClienteDao implements ClienteDao{
+	
 		public void CadastrarCliente(Cliente cliente) {
 			Connection conexao = 
 					ConnectionFactory.createConnection();
--- src/bra/ifsp/farmasisava/bd/JDBCVenda.java
+++ src/bra/ifsp/farmasisava/bd/JDBCVenda.java
@@ -35,7 +35,9 @@ public class JDBCVenda implements VendaDao {
 			    int idVenda = rs.getInt(1);
 			
 				String sql2;
+				String sql3;/*string para dar baixa na quantidade do estoque*/
 				PreparedStatement comando2;
+				PreparedStatement comando3;
 				for (ItemVenda item : itens) {
 				 sql2 = "Insert INTO itensvendidos (idVenda,idMedicamento,quantidade) Values(?,?,?)";
 				 comando2 = conexao.prepareStatement(sql2);
@@ -43,6 +45,11 @@ public class JDBCVenda implements VendaDao {
 					comando2.setInt(2,item.getMedicamento().getIdMedicamento());
 					comando2.setInt(3,item.getQuantidade());
 				comando2.execute();
+				sql3 = "UPDATE estoque SET quantidade = estoque.quantidade - ? WHERE estoque.idMedicamento = ?";
+				comando3 = conexao.prepareStatement(sql3);
+				comando3.setInt(1, item.getQuantidade());
+				comando3.setInt(2, item.getMedicamento().getIdMedicamento());
+				comando3.execute();
 				}
 			}
 			
